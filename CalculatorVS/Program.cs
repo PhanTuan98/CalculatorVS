092ce535c718ebe4f6016202feb492e86d052b9a
Jeffrey Nguyen <jeffrey.nguyen98@gmail.com>
2021-03-22 10:56:30 +0700

    1st

---
+++ CalculatorVS/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CalculatorVS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Calculator
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+    }
+}
---
+++ NUnit.Test/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using CalculatorVS;
+using NUnit.Framework;
+
+namespace NUnit.Test
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        private Calculator _cal;
+
+        [SetUp]
+        public void Setup()
+        {
+            _cal = new Calculator();
+        }
+
+        [Test]
+        public void OnePlusOneEqualTwo()
+        {
+            Assert.AreEqual(2, _cal.Add(1, 1));
+        }
+
+        [Test]
+        public void TwoPlusTwoEqualFour()
+        {
+            Assert.AreEqual(4, _cal.Add(2, 2));
+        }
+
+        [Test]
+        public void FourPlusOneEqualFive()
+        {
+            Assert.AreEqual(5, _cal.Add(4, 1));
+        }
+    }
+}
